f9c2f1191534abdeed8ebca63b45c07d45514cbb
Ariel Betel <arielbetel7@gmail.com>
2026-07-03 16:42:34 +0300

    find the com is now a function

--- Code/Change_Volume.py
+++ Code/Change_Volume.py
@@ -1,11 +1,12 @@
 import serial.tools.list_ports
 
-objects = serial.tools.list_ports.comports()
+def get_com():
+    objects = serial.tools.list_ports.comports()
 
-for i in objects:
-    print(i.device)
-    print(i.description)
+    for i in objects:
+        print(i.device)
+        print(i.description)
 
-serial.Serial("COM3", 115200, timeout=1)
-while(True):
-    print(serial.Serial("COM3", 115200, timeout=1).readline())
+    serial.Serial("COM3", 115200, timeout=1)
+    while(True):
+        print(serial.Serial("COM3", 115200, timeout=1).readline())
