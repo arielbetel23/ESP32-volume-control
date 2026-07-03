d90da8553ec2cf122b3de74e56fcf3b6fdcce2b1
Ariel Betel <arielbetel7@gmail.com>
2026-07-03 17:30:50 +0300

    Wire handle_command into read_serial, connect serial input to volume control

--- Code/Change_Volume.py
+++ Code/Change_Volume.py
@@ -2,35 +2,55 @@ import serial
 import serial.tools.list_ports
 from pycaw.pycaw import AudioUtilities
 
-
-def get_volume_interface():
-    devices = AudioUtilities.GetSpeakers()
-    device = AudioUtilities.GetSpeakers()
-    return device.EndpointVolume
-    
-
-volume = get_volume_interface()
-print(volume.GetMasterVolumeLevelScalar())
-volume.VolumeStepDown(None)
-print(volume.GetMasterVolumeLevelScalar())
-
-
-
-
-
-
-
 def list_ports():
     ports = serial.tools.list_ports.comports()
     for port in ports:
         print(port.device)
         print(port.description)
 
+def get_volume_interface():
+    device = AudioUtilities.GetSpeakers()
+    return device.EndpointVolume
+    
 def read_serial():
     ser = serial.Serial("COM3", 115200, timeout=1)
+    volume = get_volume_interface()
     while True:
         line = ser.readline()
-        print(line.decode())
+        handle_command(line.decode(), volume)
+
+def handle_command(line, volume):
+    line = line.strip()
+    if (line == "VOL:+"):
+       volume.VolumeStepUp(None)
+    elif (line == "VOL:-"):
+       volume.VolumeStepDown(None)
+    elif (line == "VOL:MUTE"):
+       volume.SetMute(1, None)
+
+
+read_serial()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
