cd7228592cc51c23e60fe5964d68488e3b47349e
Ariel Betel <arielbetel7@gmail.com>
2026-07-03 16:44:58 +0300

    Fix serial connection to open once instead of per-loop, split into list_ports and read_serial functions

--- Code/Change_Volume.py
+++ Code/Change_Volume.py
@@ -1,12 +1,15 @@
+import serial
 import serial.tools.list_ports
 
-def get_com():
-    objects = serial.tools.list_ports.comports()
+def list_ports():
+    ports = serial.tools.list_ports.comports()
+    for port in ports:
+        print(port.device)
+        print(port.description)
 
-    for i in objects:
-        print(i.device)
-        print(i.description)
+def read_serial():
+    ser = serial.Serial("COM3", 115200, timeout=1)
+    while True:
+        line = ser.readline()
+        print(line.decode())
 
-    serial.Serial("COM3", 115200, timeout=1)
-    while(True):
-        print(serial.Serial("COM3", 115200, timeout=1).readline())
