98f74bd2002db827bc7184295f4bcf82a3b941c7
Ariel Betel <arielbetel7@gmail.com>
2026-07-04 16:16:15 +0300

    created a function that reads the port and change the volume

--- Code/Main.py
+++ Code/Main.py
@@ -3,3 +3,37 @@ import threading
 import serial
 from Change_Volume import find_esp_port, get_volume_interface, handle_command
 from Send_Data import send_time_and_date, send_weather
+
+
+def reader(ser, volume):
+    while True:
+        line = ser.readline().decode(errors="ignore")
+        if line:
+            handle_command(line, volume)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
