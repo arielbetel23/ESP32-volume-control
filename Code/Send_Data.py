854ce29f86aa683061881c7f776a7b1a79fd52fa
Ariel Betel <arielbetel7@gmail.com>
2026-07-04 12:45:42 +0300

    addded a function that sends the weather to the ESP32

--- Code/Send_Data.py
+++ Code/Send_Data.py
@@ -4,12 +4,16 @@ from Change_Volume import find_esp_port
 from Get_Weather import extract_weather
 from Get_Datetime import get_date_and_time
 
-def send_time(ser):
+
+def send_time_and_date(ser):
     date, time_str = get_date_and_time()
     ser.write(f"DATE:{date}\n".encode())
     ser.write(f"TIME:{time_str}\n".encode())
 
-
+def send_weather(ser):
+    temp, desc = extract_weather()
+    ser.write(f"TEMP:{temp}\n".encode())
+    ser.write(f"DESC:{desc}\n".encode())
 
 
 
