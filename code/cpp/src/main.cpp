fe43299ea70e1e2c0aa71b799c948147e1c08eaf
arielbetel23 <arielbetel7@gmail.com>
2026-07-07 14:03:04 +0300

    defined encoder pins

--- code/cpp/src/main.cpp
+++ code/cpp/src/main.cpp
@@ -2,6 +2,9 @@
 
 const byte previous_page_button_pin = 18;
 const byte next_page_button_pin = 19;
+const byte encoder_CLK_A_pin = 32; 
+const byte encoder_DT_B_pin = 33;
+const byte encoder_switch_pin = 25;
 
 void setup(){
     pinMode(previous_page_button_pin, INPUT);
