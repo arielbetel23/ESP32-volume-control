0234a8d125c295282f6e291f9987c3e19e369c90
arielbetel23 <arielbetel7@gmail.com>
2026-07-07 13:56:54 +0300

    button pins as INPUT in setup

--- code/cpp/src/main.cpp
+++ code/cpp/src/main.cpp
@@ -4,7 +4,8 @@ const byte previous_page_button_pin = 18;
 const byte next_page_button_pin = 19;
 
 void setup(){
-
+    pinMode(previous_page_button_pin, INPUT);
+    pinMode(next_page_button_pin, INPUT);
 }
 
 void loop(){
